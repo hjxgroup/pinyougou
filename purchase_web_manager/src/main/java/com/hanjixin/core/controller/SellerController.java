05fb17ad8e9303437b3d5332aa4eba08aac8e1e7
Git <575502005@qq.com>
2019-05-10 15:18:59 +0800

    D-4001   优化了运营商后台-用户（商品、订单）数据导出

--- purchase_web_manager/src/main/java/com/hanjixin/core/controller/SellerController.java
+++ purchase_web_manager/src/main/java/com/hanjixin/core/controller/SellerController.java
@@ -6,14 +6,12 @@ import com.hanjixin.core.pojo.seller.Seller;
 import com.hanjixin.core.service.SellerService;
 import entity.PageResult;
 import entity.Result;
-import org.apache.poi.hssf.eventmodel.ERFListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.UUID;
 
@@ -23,21 +21,27 @@ import java.util.UUID;
 public class SellerController {
         @Reference
         private SellerService sellerService;
-
+    @Value("${files}")
+    private String files;
+    @Value("${columnName}")
+    private String columnName;
+    @Value("${key}")
+    private String key;
     //查询 带分页 待条件
     @RequestMapping("/search")
     public PageResult search(Integer page, Integer rows, @RequestBody Seller seller){
         return sellerService.search(page,rows,seller);
     }
+
     //批量导出数据
     @RequestMapping("/dataExport")
     public Result dataExport(HttpServletResponse response) {
 
         try {
             List<Seller> sellerList = sellerService.dataExport();
-            String fileName = "商家详细信息" + UUID.randomUUID().toString().replaceAll("-","") + ".xls";
-            String[] columnNames = {"用户ID", "公司名称", "店铺名称", "email", "公司手机", "公司电话", "状态", "详细地址", "联系人姓名"};
-            String[] keys = {"sellerId","name","nickName","email","mobile","telephone","status","addressDetail","linkmanName"};
+            String fileName = files + UUID.randomUUID().toString().replaceAll("-","") + ".xls";
+            String[] columnNames = columnName.split(",");
+            String[] keys = key.split(",");
             ExportPOIUtils.start_download(response, fileName, sellerList, columnNames, keys);
             return new Result(true,"数据导出成功!!");
         } catch (IOException e) {
